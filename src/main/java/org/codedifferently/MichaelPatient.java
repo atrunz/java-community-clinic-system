b0af17fa57d6ba3ccdaf5c5900e24e052feaad7e
michaelmoss <mxmoss261@gmail.com>
2026-02-19 16:47:05 -0500

    michaelmoss java community clinic system

--- src/main/java/org/codedifferently/MichaelPatient.java
+++ src/main/java/org/codedifferently/MichaelPatient.java
@@ -6,23 +6,36 @@ public class MichaelPatient {
     private String ID;
     private boolean isCheckedIn;
 
-    MichaelPatient(String Name, String ID, boolean isCheckedIn) {
-        this.name=Name;
-        this.ID=ID;
-        this.isCheckedIn=false;
+        public MichaelPatient(String name, String ID) {
+            this.name = name;
+            this.ID = ID;
+            this.isCheckedIn = false;
+        }
 
+        public String getName() {
+            return name;
+        }
 
+        public String getID() {
+            return ID;
+        }
+
+        public boolean isCheckedIn() {
+            return isCheckedIn;
+        }
+
+        public void setCheckedIn(boolean checkedIn) {
+            this.isCheckedIn = checkedIn;
+        }
+
+        @Override
+        public String toString() {
+            return "ID: " + ID +
+                    " | Name: " + name +
+                    " | Checked In: " + isCheckedIn;
+        }
     }
 
-    public void setName(String name) {
-        this.name=name;
 
-    }
 
-    public void setID(String ID) {
-        this.ID=ID;
 
-    }
-//
-     //
-}
