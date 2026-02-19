750aff65f973f2451ad9a9caf3c545893b97b5d4
michaelmoss <mxmoss261@gmail.com>
2026-02-19 15:47:31 -0500

    ibitial patient class

---
+++ src/main/java/org/codedifferently/MichaelPatient.java
@@ -0,0 +1,27 @@
+package org.codedifferently;
+
+public class MichaelPatient {
+
+    private String name;
+    private String ID;
+    private boolean isCheckedIn;
+
+    MichaelPatient(String Name, String ID, boolean isCheckedIn) {
+        this.name=Name;
+        this.ID=ID;
+        this.isCheckedIn=false;
+
+
+    }
+
+    public void setName(String name) {
+        this.name=name;
+
+    }
+
+    public void setID(String ID) {
+        this.ID=ID;
+
+    }
+
+}
