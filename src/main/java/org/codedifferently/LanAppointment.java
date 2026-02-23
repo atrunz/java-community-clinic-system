4ca33155489817a20def4cf79ec5a51fb857b7dc
atrunz <atrunzo261@gmail.com>
2026-02-23 02:56:35 -0500

    modifying date functionality

--- src/main/java/org/codedifferently/LanAppointment.java
+++ src/main/java/org/codedifferently/LanAppointment.java
@@ -1,5 +1,6 @@
 package org.codedifferently;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class LanAppointment {
@@ -7,15 +8,18 @@ public class LanAppointment {
     private String patientID;
     private LocalDateTime time;
     private boolean isCompleted;
+    private LocalDate date;
 
-    public LanAppointment(String patientID, LocalDateTime time) {
+    public LanAppointment(String patientID, LocalDateTime time, LocalDate date) {
         this.patientID = patientID;
         this.time = time;
+        this.date = date;
         this.isCompleted = false;
     }
 
     public String getPatientID() { return patientID; }
     public LocalDateTime getTime() { return time; }
+    public LocalDate getDate() { return date;}
     public boolean isCompleted() { return isCompleted; }
 
     public void complete() { isCompleted = true; }
